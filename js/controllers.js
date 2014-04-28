0bffc8158aa45f403dcc4fd8a65dcdf593cb7321
DzikuVx <pspychalski@gmail.com>
2014-04-28 18:03:29 +0200

    jQuery replaced with Angular UI.Bootstrap

--- js/controllers.js
+++ js/controllers.js
@@ -1,31 +1,9 @@
-var apcBrowserApp = angular.module('apcBrowser', []);
+var apcBrowserApp = angular.module('apcBrowser', ['ui.bootstrap']);
 
 apcBrowserApp.controller('EntryListController', function ($scope, $http) {
-//    $scope.entries = [
-//        {
-//            key: 'key1',
-//            'hits': 2,
-//            'size' : 12,
-//            'accessed': '2012-01-01',
-//            'modified': '2012-01-01',
-//            'created': '2012-01-01',
-//            'timeout': '84600'
-//        }, {
-//            'key': 'key2',
-//            'hits': 2,
-//            'size' : 12,
-//            'accessed': '2012-01-01',
-//            'modified': '2012-01-01',
-//            'created': '2012-01-01',
-//            'timeout': '84600'
-//        }
-//    ];
 
     $http.get('api.php?controller=EntryList').success(function(data) {
         $scope.entries = data;
     });
-
-//    $scope.orderProp = 'age';
-//    api.php?controller=EntryList
 
 });
